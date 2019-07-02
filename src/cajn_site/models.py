4c68c92551c07b619c29908e89eea51ecc078ca0
Jayden <jstoll11@gmail.com>
2019-07-01 21:08:03 -0600

    Client creation basics working. All needed is to flesh out the database and update the form. Also allow deletion of a specific client from the database. styling still required aswell as making it so each user will get their own database instead of making it a shared database

--- src/cajn_site/models.py
+++ src/cajn_site/models.py
@@ -1,11 +1,9 @@
 from django.db import models
 
-
-class Vehicles(models.Model):
-    serial_num = models.TextField()
+class Vehicle(models.Model):
+    vehicle_type = models.TextField()
 
 class Clients(models.Model):
     # set these to proper model data structure
     client_f_name = models.TextField()
     client_l_name = models.TextField()
-    vehicles = Vehicles()
--- src/cajn_site/views.py
+++ src/cajn_site/views.py
@@ -1,12 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+
+from .models import Clients
 
 def home(request):
     return render(request, 'site/home.html')
 
 def ManageClients(request):
-    ############ ToDo ##################
-    #  1. Get the client information and put into array
-    #  2. print out the first and last name into clickable links/buttons
-    #  3. when client is clicked display vehicles 
-    return render(request, 'site/clients-pages/manage-clients.html')
+    all_clients = Clients.objects.all()
+    return render(request, 'site/clients-pages/manage-clients.html', {'all_clients': all_clients})
+
+
+######################### Client Pages ###########################
+
+def AddClientView(request):
+    return render(request, 'site/add-info-pages/add-client.html')
+
+def AddClient(request):
+    new_item = Clients(client_f_name = request.POST["f_name"], client_l_name = request.POST["l_name"])
+    new_item.save()
+    return redirect('cajn-manage-clients')
+
+def ClientManagePage(request, client_id):
+    client_info = Clients.objects.get(pk = client_id)
+    return render(request, 'site/clients-pages/client-info.html', {'client_info': client_info})
+
+def DeleteClient(request, id):
+    client_info = Clients.objects.get(pk = id)
+    client_info.delete()
+    return redirect('cajn-manage-clients')
