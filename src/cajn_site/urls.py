d0ac8b5c3e87c752faebda1516177f00c12a952b
Jayden <jstoll11@gmail.com>
2019-08-22 16:40:04 -0600

    removed single views file and moved to seperate folders for cleaner file structure. added a logout so now user will not always remain logged in fixed bug where new clients couldnt be added to the database

--- src/cajn_site/urls.py
+++ src/cajn_site/urls.py
@@ -1,15 +1,16 @@
 from django.urls import path
-from . import views
+from .views import base_views as base, client_views as client, vehicle_views as vehicle
 
 urlpatterns = [
     # visible page routes
-    path('', views.home, name="cajn-site-home"),
-    path('manage-clients/', views.ManageClients, name='cajn-manage-clients'),
-    path('add-client/', views.AddClientView, name='cajn-add-client'),
-    path('client-info/<int:client_id>', views.ClientManagePage, name='cajn-client'),
-    path('add-vehicle/', views.AddVehicleView, name='cajn-add-vehicle'),
+    path('', base.home, name="cajn-site-home"),
+    path('manage-clients/', base.ManageClients, name='cajn-manage-clients'),
+    path('add-client/', client.AddClientView, name='cajn-add-client'),
+    path('client-info/<int:client_id>', client.ClientManagePage, name='cajn-client'),
+    path('add-vehicle/', vehicle.AddVehicleView, name='cajn-add-vehicle'),
 
     # add info to db routes
-    path('DeleteClient/<int:id>', views.DeleteClient),
-    path('EditClient/<int:id>', views.EditClient),
+    path('DeleteClient/<int:id>', client.DeleteClient),
+    #path('EditClient/<int:id>', views.EditClient),
+    path('AddVehicleClient/<int:id>', client.AddVehicleToClient),
 ]
---
+++ src/cajn_site/views/__init__.py
@@ -0,0 +1,3 @@
+from .base_views import *
+from .client_views import *
+from .vehicle_views import *
